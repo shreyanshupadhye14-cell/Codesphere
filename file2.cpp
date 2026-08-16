20cc9070bcc85c0d0ff14070695efa77e15aa405
shreyanshupadhye14-cell <shreyanshupadhye14@gmail.com>
2026-08-16 07:39:40 +0000

    file2.cpp

--- file2.cpp
+++ file2.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
-namespace syc:
+namespace syc
 {
     int show();
 int main()
-{
-    cout << "I am in namespace syc";
+ {
+   cout << "I am in namespace syc";
     return 0;
-}
-}
+ }
+} 
