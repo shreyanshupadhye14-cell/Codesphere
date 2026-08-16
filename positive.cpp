985dbad34500d33209ed0fe2ad238f6a108ba685
shreyanshupadhye14-cell <shreyanshupadhye14@gmail.com>
2026-08-16 09:20:39 +0000

    positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if(n > 0)
+    {
+        cout << "Number is Positive";
+    }
+    else if(n < 0)
+    {
+        cout << "Number is Negative";
+
+    }
+    else
+    {
+        cout << "Number is zero";
+    }
+    return 0;
+}
