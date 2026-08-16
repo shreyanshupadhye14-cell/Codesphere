a54dbfbfb95b65845b2c6edda920b4d0c54f6da7
shreyanshupadhye14-cell <shreyanshupadhye14@gmail.com>
2026-08-16 07:24:28 +0000

    file1.cpp

---
+++ file1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+
+ int add()
+ {
+     cout << "my name is samrudhi:" << endl;
+     cout << "I am in namespce syc:" << endl;
+     return 0;
+ }
+ int main()
+ {
+    add();
+    return 0;
+ }
+ 
